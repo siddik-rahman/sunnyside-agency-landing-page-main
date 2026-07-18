1b4992023c706ec5a12c7b9fe7374bbae72c51c7
siddik-rahman <siddik8976@gmail.com>
2026-07-18 22:58:20 +0600

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/sunnyside-agency-landing-page-main/',
   plugins: [
     tailwindcss(),
   ],
